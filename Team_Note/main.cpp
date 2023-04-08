d5f531ffb97b8dd85fe73ce849f81b80f7d6f40a
dosawas <dosawas@naver.com>
2023-04-08 11:17:09 +0900

    Initial Commit

---
+++ Team_Note/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Team_Note
+//
+//  Created by 한승준 on 2023/04/08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
